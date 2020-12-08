9a5c11f792a0907c4a59bb9c0c43b08989907cea
Filip <filip.dorocinski@gmail.com>
2020-12-08 22:40:15 +0100

    remove multithreading; run app.py through command line

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ import time
 import sys
 from datetime import datetime
 import importlib
-import threading
 
 
 
@@ -24,10 +23,6 @@ tgt_database = 'MYSQL_DB' #also alias for KeePass entry in KeePass_config.py
 tgt_transport_api_table = 'pz1000bus_tram'
 tgt_weather_api_table = 'pz2000actual_weather'
 tgt_maps_table = 'pz3000traffic_data'
-
-# transport_data_get_interval = 30
-# weather_data_get_interval = 30
-# traffic_data_get_interval = 30
 
 #Transport api params
 transport_api_params = [
@@ -49,125 +44,135 @@ try:
     from libs.lib_database import db_clients, dbc
     from libs.lib_screenshots import screen_analyzer
 
-    start = time.time()
     kp = cred.JSONpassDB()
-    logger.info(f'Opening pass database took {time.time() - start}')
     api_configs = importlib.import_module(f'configs.{transport_operator}')
 
-    transport_api_config = api_configs.transport_api_config.ConfigureTransportAPI()
-    weather_api_config = api_configs.weather_api_config.ConfigureWeatherAPI()
-    maps_config = api_configs.maps_config.ConfigureMaps()
-    from configs.screenshots.selenium_config import selenium_config
-    from configs.screenshots.screenshot_config import screen_analyzer_config
-
+    # transport_api_config = api_configs.transport_api_config.ConfigureTransportAPI()
+    # weather_api_config = api_configs.weather_api_config.ConfigureWeatherAPI()
+    # maps_config = api_configs.maps_config.ConfigureMaps()
+    # from configs.screenshots.selenium_config import selenium_config
+    # from configs.screenshots.screenshot_config import screen_analyzer_config
+    #
     db_config = getattr(importlib.import_module(f'configs.databases.database_config'), f'{tgt_database}')
-    tgt_transport_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_transport_api_table}')
-    tgt_transport_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_transport_api_table}')
-    tgt_transport_table_config = dbc.TableInfo(*tgt_transport_table_meta, tgt_transport_table_parser)
-
-    tgt_weather_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_weather_api_table}')
-    tgt_weather_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_weather_api_table}')
-    tgt_weather_table_config = dbc.TableInfo(*tgt_weather_table_meta, tgt_weather_table_parser)
-
-    tgt_maps_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_maps_table}')
-    tgt_maps_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_maps_table}')
-    tgt_maps_table_config = dbc.TableInfo(*tgt_maps_table_meta, tgt_maps_table_parser)
+    # tgt_transport_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_transport_api_table}')
+    # tgt_transport_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_transport_api_table}')
+    # tgt_transport_table_config = dbc.TableInfo(*tgt_transport_table_meta, tgt_transport_table_parser)
+    #
+    # tgt_weather_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_weather_api_table}')
+    # tgt_weather_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_weather_api_table}')
+    # tgt_weather_table_config = dbc.TableInfo(*tgt_weather_table_meta, tgt_weather_table_parser)
+    #
+    # tgt_maps_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_maps_table}')
+    # tgt_maps_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser') ,f'{tgt_maps_table}')
+    # tgt_maps_table_config = dbc.TableInfo(*tgt_maps_table_meta, tgt_maps_table_parser)
 
 except Exception as e:
     logger.error(f'Initing configs failed with {e}', exc_info=True)
     print(f'Initing configs failed with {e}')
     sys.exit()
 
-#init clients objects
-transport_api_client = api.LiveDataClient(kp, transport_operator, transport_api_config)
-transport_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_transport_table_config)
-weather_api_client = api.LiveDataClient(kp, weather_provider, weather_api_config)
-weather_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_weather_table_config)
-maps_client = screen_analyzer.ScreenAnalyzer(selenium_config, screen_analyzer_config)
-maps_table_client  = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_maps_table_config)
+# #init clients objects
+# transport_api_client = api.LiveDataClient(kp, transport_operator, transport_api_config)
+# transport_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_transport_table_config)
+# weather_api_client = api.LiveDataClient(kp, weather_provider, weather_api_config)
+# weather_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_weather_table_config)
+# maps_client = screen_analyzer.ScreenAnalyzer(selenium_config, screen_analyzer_config)
+# maps_table_client  = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_maps_table_config)
 
 ################################# PROGRAM EXECUTION #todo create separate file for continous functions
 #transport data
-def transport_data_get_continously(stop_event,):
-    while not stop_event.wait(1):
-        thread_start = time.time()
-        for param in transport_api_params:
-            start = time.time()
-            transport_data = transport_api_client.get_data(param)[transport_api_config.JSON_RESULT_LABEL]
-            logger.info(f'Getting transport data took {time.time() - start}')
+def transport_data_get_continously():
+    try:
+        transport_api_config = api_configs.transport_api_config.ConfigureTransportAPI()
 
-            start = time.time()
-            for elem in transport_data:
-                transport_api_table_client.insert_json(elem)
-            logger.info(f'Inserted {len(transport_data)} rows with transport data.Inserting took {time.time() - start}')
-        logger.info(f'Transport thread going sleep for {transport_data_get_interval - (time.time() - thread_start)} seconds.')
-        thread_time = (time.time() - thread_start) if transport_data_get_interval - (time.time() - thread_start) > 0 else 10
-        time.sleep(transport_data_get_interval - thread_time)
-        logger.info(f'Transport thread wakes up.')
-    logger.info(f'Finishing collecting transport data on user request.')
-    print(f'Finishing collecting transport data on user request.')
+        tgt_transport_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_transport_api_table}')
+        tgt_transport_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser'), f'{tgt_transport_api_table}')
+        tgt_transport_table_config = dbc.TableInfo(*tgt_transport_table_meta, tgt_transport_table_parser)
+
+    except Exception as e:
+        logger.error(f'Initing configs failed with {e}', exc_info=True)
+        print(f'Initing configs failed with {e}')
+        sys.exit()
+
+    transport_api_client = api.LiveDataClient(kp, transport_operator, transport_api_config)
+    transport_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_transport_table_config)
+
+    for param in transport_api_params:
+        start = time.time()
+        transport_data = transport_api_client.get_data(param)[transport_api_config.JSON_RESULT_LABEL]
+        logger.info(f'Getting transport data took {time.time() - start}')
+
+        start = time.time()
+        for elem in transport_data:
+            transport_api_table_client.insert_json(elem)
+        logger.info(f'Inserted {len(transport_data)} rows with transport data.Inserting took {time.time() - start}')
+
 
 #weather data
-def weather_data_get_continously(stop_event,):
-    while not stop_event.wait(1):
-        weather_data=[]
-        start = time.time()
-        for measure_point in weather_api_config.measure_points_coordinates:
-            weather_data.append(weather_api_client.get_data(measure_point))
-        logger.info(f'Getting weather data took {time.time() - start}')
+def weather_data_get_continously():
+    try:
+        weather_api_config = api_configs.weather_api_config.ConfigureWeatherAPI()
 
-        start = time.time()
-        for elem in (weather_data):
-            weather_api_table_client.insert_json(elem) #_bulk off #TODO fix bulk insert
-        logger.info(f'Inserting weather data took {time.time() - start}')
-        logger.info(f'Weather thread going sleep for {weather_data_get_interval} seconds.')
-        for i in range(int(weather_data_get_interval/10)):
-            time.sleep(10)
-        logger.info(f'Weather thread wakes up.')
-    logger.info(f'Finishing collecting weather data on user request.')
-    print(f'Finishing collecting weather data on user request.')
+        tgt_weather_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_weather_api_table}')
+        tgt_weather_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser'), f'{tgt_weather_api_table}')
+        tgt_weather_table_config = dbc.TableInfo(*tgt_weather_table_meta, tgt_weather_table_parser)
+
+    except Exception as e:
+        logger.error(f'Initing configs failed with {e}', exc_info=True)
+        print(f'Initing configs failed with {e}')
+        sys.exit()
+
+    weather_api_client = api.LiveDataClient(kp, weather_provider, weather_api_config)
+    weather_api_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_weather_table_config)
+
+    weather_data=[]
+    start = time.time()
+    for measure_point in weather_api_config.measure_points_coordinates:
+        weather_data.append(weather_api_client.get_data(measure_point))
+    logger.info(f'Getting weather data took {time.time() - start}')
+
+    start = time.time()
+    for elem in (weather_data):
+        weather_api_table_client.insert_json(elem) #_bulk off #TODO fix bulk insert
+    logger.info(f'Inserting weather data took {time.time() - start}')
+
 
 #traffic
-def traffic_data_get_continously(stop_event,):
-    while not stop_event.wait(1):
-        start = time.time()
-        for elem, place in zip(maps_config.urls, maps_config.places):
-            traffic_data = maps_client.get_screen(elem).get_image_pixels(place, datetime.now())
-            maps_table_client.insert_json_bulk(traffic_data)
-        logger.info(f'Gathering traffic data took {time.time() - start}')
-        logger.info(f'Traffic thread going sleep for {traffic_data_get_interval} seconds.')
-        for i in range(int(traffic_data_get_interval/10)):
-            time.sleep(10)
-        logger.info(f'Traffic thread wakes up.')
-    logger.info(f'Finishing collecting traffic data on user request.')
-    print(f'Finishing collecting traffic data on user request.')
+def traffic_data_get_continously():
+    try:
+        maps_config = api_configs.maps_config.ConfigureMaps()
+        from configs.screenshots.selenium_config import selenium_config
+        from configs.screenshots.screenshot_config import screen_analyzer_config
 
-def user_end(stop_event,):
-    while not stop_event.wait(1):
-        if input('Type EXIT to end program execution:') == 'EXIT':
-            logger.info('User requested to stop execution')
-            pill2kill.set()
-    print('Finishing program.')
-    logger.info('Finishing program.')
+        tgt_maps_table_meta = getattr(importlib.import_module(f'configs.databases.tables_config'), f'{tgt_maps_table}')
+        tgt_maps_table_parser = getattr(importlib.import_module(f'configs.{transport_operator}.parser'),
+                                        f'{tgt_maps_table}')
+        tgt_maps_table_config = dbc.TableInfo(*tgt_maps_table_meta, tgt_maps_table_parser)
+
+    except Exception as e:
+        logger.error(f'Initing configs failed with {e}', exc_info=True)
+        print(f'Initing configs failed with {e}')
+        sys.exit()
+
+    maps_client = screen_analyzer.ScreenAnalyzer(selenium_config, screen_analyzer_config)
+    maps_table_client = db_clients.DatabaseObjectClient(kp, tgt_database, db_config, tgt_maps_table_config)
+
+    start = time.time()
+    for elem, place in zip(maps_config.urls, maps_config.places):
+        traffic_data = maps_client.get_screen(elem).get_image_pixels(place, datetime.now())
+        maps_table_client.insert_json_bulk(traffic_data)
+    logger.info(f'Gathering traffic data took {time.time() - start}')
 
 
-pill2kill = threading.Event()
-tasks = [transport_data_get_continously,
-         weather_data_get_continously,
-         traffic_data_get_continously,
-         user_end]
 
-def thread_gen(pill2kill, tasks):
-    for task in tasks:
-        t = threading.Thread(target=task, args=(pill2kill,))
-        yield t
+if __name__ == "__main__":
+    data_source = (sys.argv[1])
+    {
+        'transport': transport_data_get_continously,
+        'weather': weather_data_get_continously,
+        'traffic': traffic_data_get_continously
+    }[data_source]()
 
-threads = list(thread_gen(pill2kill, tasks))
 
-for thread in threads:
-    thread.start()
-
-for thread in threads:
-    thread.join()
 
 logger.info('Program finished!')
