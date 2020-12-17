e9f27ca1183fb6680627b0b144030be7fbb207f5
Filip <filip.dorocinski@gmail.com>
2020-12-17 19:28:55 +0100

    modify ODBC driver name

--- configs/databases/database_config.py
+++ configs/databases/database_config.py
@@ -3,7 +3,7 @@ from libs.lib_database import dbConnections as dbc
 #DB connections
 MYSQL_DB = dbc.ServerInfo(
     "",
-    "DRIVER=MySQL ODBC 8.0 ANSI Driver;SERVER=94.152.11.197:3306;DATABASE={database};UID={username};PWD={password};",
+    "DRIVER=MySQL ODBC 8.0;SERVER=94.152.11.197:3306;DATABASE={database};UID={username};PWD={password};",
     "baza22697_prognoztm_stage",
     dbc.ServerType.MYSQL
 )
