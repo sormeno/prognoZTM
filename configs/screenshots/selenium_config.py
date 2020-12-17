9ad09937bd49d448619d308bb0243f7cf88651bc
Filip <filip.dorocinski@gmail.com>
2020-12-17 20:04:35 +0100

    add selenium options

--- configs/screenshots/selenium_config.py
+++ configs/screenshots/selenium_config.py
@@ -1,7 +1,9 @@
 selenium_config ={
     'config' : [
         "--headless",
-        "--window-size=3840x2160"
+        "--window-size=3840x2160",
+        "--no-sandbox",
+        "--disable-dev-shm-usage"
     ],
     'browser_driver': 'chromedriver/chromedriver'
 }
